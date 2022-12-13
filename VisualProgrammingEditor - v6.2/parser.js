3542a841912759fb5977818de8c5be121f98a90a
Lefteris Kotsonas <lefteriskotsonas2@gmail.com>
2022-12-14 00:51:42 +0200

    Need Fixes - Complete direct linking to Decl and Obj
    Doesnt make sense to connect directly to functions since we may need to define the function as a whole.

--- VisualProgrammingEditor - v6.2/parser.js
+++ VisualProgrammingEditor - v6.2/parser.js
@@ -50,18 +50,18 @@ function createAST(nodes, links) {
             for(j=0 ; j < linkItem.length; j++) {
                 const parentItem = myNodeMap.get(linkItem[j].from);
                 console.log(parentItem)
-                if(parentItem) {
+                if(parentItem) { // Built-In
                     if (linkItem[j].category == "Reversed") {
                         parentItem.items = [];
                         parentItem.items = item.items;
                     }
-                    else {
+                    else { // Manulally linked
                         if(linkItem[j].fromPort == "( )") {
                             parentItem.items.push({portId: "( )", connectedBlock: true})
                         }
                         var index = parentItem.items.map(function(e) { return e.portId}).indexOf(linkItem[j].fromPort)
                         if(item.type == "decl") {
-                            parentItem.items[index].argument = convertItemToVarRef(item, linkItem[j]);
+                            parentItem.items[index].argument = convertItemToRef(item, linkItem[j]);
                         }
                         else {
                             parentItem.items[index].argument = item; 
@@ -78,31 +78,95 @@ function createAST(nodes, links) {
     return tree;
 }
 
-function convertItemToVarRef(item, link) {
-    item.type = "varsRefer";
+function convertItemToRef(item, link) {
 
-    console.log(item)
-    const node = myDiagram.findNodeForKey(item.key);
-    var tableIndex = Number(link.toPort.replace("inSlot",'')) - 1
+    var tableIndex = Number(link.toPort.replace("inSlot",'')) - 1; 
 
-    const newParamText = item.items[tableIndex].variable;
+    const newParamText = item.items[tableIndex].variable; // get paramtext
 
-    indexKey = 0;
-    while(myDiagram.findNodeForKey(indexKey)) {
-        indexKey++;
+    const functionNode = getParentNode(item); //get parent of decl node
+
+    item.key = getUniqueKey(); // set key
+
+    if(functionNode.data.type == "varsDecl") {
+        item.type = "varsRefer"; // set type
+        item.items = [
+            {
+                "portId": "var",
+                "paramtext": newParamText
+            }
+        ];
     }
-    item.key = indexKey;
-
-    item.items = [
-        {
-            "portId": "var",
-            "paramtext": newParamText
-        }
-    ]
-
+    else {
+        item.type = "getElem";
+        item.items = [
+            {
+                "paramtext": newParamText
+            }
+        ];
+        let inLinks = functionNode.findLinksInto(); // there is only one for function box
+        setGetElem_Items(inLinks.first(), item);
+        setGetElem_PortIds(item);
+    }
     return item;
 }
 
-function convertItemToGetElem(item, link) {
+function getParentNode(item) {
+    var parentNode;
+    const node = myDiagram.findNodeForKey(item.key); //get node instead of data
+
+    const inLinks = node.findLinksInto();
+
+    var linkIterator = inLinks.iterator;
+
+    while(linkIterator.next()) {
+        var i_item = linkIterator.value;
+        parentNode = myDiagram.findNodeForKey(i_item.data.from)
+
+        if(parentNode.data.type == "varsDecl" || parentNode.data.type == "object" ) { // dont forget assign
+            break;
+        }
+    }
+
+    return parentNode;
+}
+
+function getUniqueKey() {
+    let indexKey = 0;
+    while(myDiagram.findNodeForKey(indexKey)) {
+        indexKey++;
+    }
+    return indexKey;
+}
+
+function setGetElem_Items(linkToParent, item) {
+    var parentNode;
+    var parentFunctionNode;
+    var tableIndex = Number(linkToParent.data.fromPort) - 1; 
+
+    parentNode = myDiagram.findNodeForKey(linkToParent.data.from) // get parent of functionNode (decl arguments)
+    parentFunctionNode = getParentNode(parentNode); // get parent of parentNode ( Vars decl / Object )
+
+    console.log(tableIndex);
+    const newParamText = parentNode.data.items[tableIndex].variable; // get paramtext
+
+    item.items.unshift({
+        "paramtext": newParamText
+    }) // set item to beginning of items array
+
+    if(parentFunctionNode.data.type == "varsDecl") { return; }
+    let inLinks = parentFunctionNode.findLinksInto(); // there is only one for function box
+    setGetElem_Items(inLinks.first(), item)
+}
+
+function setGetElem_PortIds(item) {
+    item.items[0].portId = "obj";
+    let keyPrefix = "key"
+    for(var i = 1; i < item.items.length; i++) {
+        item.items[i].portId = keyPrefix + i;
+    }
+}
+
+function convertItemToCall() {
     
 }
